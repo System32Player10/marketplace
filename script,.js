fa48ffaec3050dabf425f2770126a12f05a1c6c6
System32Player <djasonlie168@gmail.com>
2026-08-08 22:18:39 +0700

    2026-08-08

---
+++ script,.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("shop").addEventListener("click", function() {
+        window.location.hash = '#start';
+    });
+});
