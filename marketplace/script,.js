13fc721d126197c08978b1a34afb13597032fe1e
System32Player10 <127583461+System32Player10@users.noreply.github.com>
2026-08-23 20:18:23 +0700

    Delete marketplace directory

--- marketplace/script,.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("shop").addEventListener("click", function() {
-        window.location.hash = '#start';
-    });
-});
